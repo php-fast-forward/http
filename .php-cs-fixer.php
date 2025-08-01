427a763e009eff778d34d5c3a6d9ca08647d4a3b
Felipe Sayão Lobato Abreu <github@mentordosnerds.com>
2025-08-01 18:18:06 -0300

    Fix code-style

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 /**
- * This file is part of php-fast-forward/http-message.
+ * This file is part of php-fast-forward/http.
  *
  * This source file is subject to the license bundled
  * with this source code in the file LICENSE.
  *
- * @link      https://github.com/php-fast-forward/http-message
+ * @link      https://github.com/php-fast-forward/http
  * @copyright Copyright (c) 2025 Felipe Sayão Lobato Abreu <github@mentordosnerds.com>
  * @license   https://opensource.org/licenses/MIT MIT License
  */
--- src/ServiceProvider/HttpServiceProvider.php
+++ src/ServiceProvider/HttpServiceProvider.php
@@ -1,5 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * This file is part of php-fast-forward/http.
+ *
+ * This source file is subject to the license bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/php-fast-forward/http
+ * @copyright Copyright (c) 2025 Felipe Sayão Lobato Abreu <github@mentordosnerds.com>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
 namespace FastForward\Http\ServiceProvider;
 
 use FastForward\Container\ServiceProvider\AggregateServiceProvider;
@@ -7,10 +20,28 @@ use FastForward\Http\Client\ServiceProvider\HttpClientServiceProvider;
 use FastForward\Http\Message\Factory\ServiceProvider\HttpMessageFactoryServiceProvider;
 use Interop\Container\ServiceProviderInterface;
 
+/**
+ * Class HttpServiceProvider
+ *
+ * Aggregates and registers HTTP-related service providers. This class SHALL encapsulate the
+ * dependencies for HTTP client and message factory services within the container.
+ *
+ * It MUST implement the ServiceProviderInterface and MUST delegate factory and extension
+ * retrieval to the internal AggregateServiceProvider instance.
+ */
 final class HttpServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * @var ServiceProviderInterface The aggregate service provider that includes all HTTP service providers.
+     */
     private ServiceProviderInterface $serviceProvider;
 
+    /**
+     * Constructs the HttpServiceProvider.
+     *
+     * This constructor MUST initialize the internal service provider as an instance of
+     * AggregateServiceProvider composed of HTTP-related service providers.
+     */
     public function __construct()
     {
         $this->serviceProvider = new AggregateServiceProvider(
@@ -19,11 +50,25 @@ final class HttpServiceProvider implements ServiceProviderInterface
         );
     }
 
+    /**
+     * Returns an array of factory callables provided by the aggregate service provider.
+     *
+     * This method MUST delegate the call to the internal AggregateServiceProvider.
+     *
+     * @return array<string, callable> A map of service identifiers to factory callables.
+     */
     public function getFactories(): array
     {
         return $this->serviceProvider->getFactories();
     }
 
+    /**
+     * Returns an array of service extension callables provided by the aggregate service provider.
+     *
+     * This method MUST delegate the call to the internal AggregateServiceProvider.
+     *
+     * @return array<string, callable> A map of service identifiers to extension callables.
+     */
     public function getExtensions(): array
     {
         return $this->serviceProvider->getExtensions();
